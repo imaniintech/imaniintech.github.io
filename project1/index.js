705059b2634ac1d2ea458f3b3b79c6baccf48f6b
ImaniW <imani@ImaniWs-MacBook-Pro.local>
2021-05-12 10:59:19 -0400

    grabbed id and classes from html doc to load into my js file.

---
+++ project1/index.js
@@ -0,0 +1,12 @@
+
+//Grab the game-board, the components, player and the outcome from html doc to load into JS
+document.addEventListener('DOMContentLoaded', () => {
+    const token = document.querySelectorAll('.game-board div')
+    const outcome = document.querySelector('#outcome')
+    const currentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+
+
+
+
+});
